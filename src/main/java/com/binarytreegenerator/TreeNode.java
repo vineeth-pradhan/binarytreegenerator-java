49ddf34b55d493f228c5e90b7a0a50811bcec5c1
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-04-17 20:26:01 +0530

    Adding accessor methods

--- src/main/java/com/binarytreegenerator/TreeNode.java
+++ src/main/java/com/binarytreegenerator/TreeNode.java
@@ -5,16 +5,15 @@ public class TreeNode {
   TreeNode left;
   TreeNode right;
 
-  TreeNode() {
-  }
-
-  TreeNode(int val) {
-    this.val = val;
-  }
-
+  TreeNode() { }
+  TreeNode(int val) { this.val = val; }
   TreeNode(int val, TreeNode left, TreeNode right) {
     this.val = val;
     this.left = left;
     this.right = right;
   }
+
+  public int getVal(){ return this.val; }
+  public TreeNode getLeft() { return this.left; }
+  public TreeNode getRight() { return this.right; }
 }
